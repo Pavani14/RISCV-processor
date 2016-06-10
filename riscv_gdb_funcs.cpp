4e94a2a816afbf0ac89e8abe1a45f842720fe201
Pavani14 <pavani13147@iiitd.ac.in>
2016-06-10 12:49:57 +0530

    Update riscv_gdb_funcs.cpp

--- riscv_gdb_funcs.cpp
+++ riscv_gdb_funcs.cpp
@@ -13,38 +13,24 @@
 using namespace riscv_parms;
 
 int riscv::nRegs(void){
-	return //look for the right number to return
+	return 33;
 }
 
 ac_word riscv::reg_read(){
 	if((reg>=0) && (reg<32))
 		return RB.read(reg);
-	else{
-		if(reg == 33)
-			return lo;
-		else if (reg == 34)
-			return hi;
-		else
-			if(reg == )
-				return ac_pc;
-	}
+	else if(reg == 33)
+		return ac_pc;
 }
 
 ac_word riscv::reg_write(){
 	if((reg>=0) && (reg<32))
-		RB.write(reg-1, value);
-	else{
-		//conditions for hi and lo
-		if ()
-		
-		else
-			if(reg == )
-				ac_pc = value;
-	}
+		RB.write(reg, value);
+	else if(reg == 33)
+		ac_pc = value;
 }
 
 
-//check these two functions
 unsigned char riscv::mem_read(){
 	return IM->read_byte(address);
 }
